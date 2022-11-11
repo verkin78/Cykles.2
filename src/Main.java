d00a802a94dfded14e40c2f997e490642a8832c7
Verkin78 <otrajenie.jc@gmail.com>
2022-11-11 11:30:54 +0300

    Cykles.2 ДЗ 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public static void main(String[] args) {
+    int deposit = 15000;
+    int total = 0;
+    int m = 1;
+    while (total < 2459000){
+    total = total + total / 100;
+    total = total + deposit;
+    System.out.println(" Месяц " + m ++ + ", сумма накоплений равна " + total + " рублей ");}
     }
 }
