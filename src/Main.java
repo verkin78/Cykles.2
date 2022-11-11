15b7546acc3976f500ac6235205af6235c163b2c
Verkin78 <otrajenie.jc@gmail.com>
2022-11-11 12:16:25 +0300

    Cykles.2 ДЗ 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,41 @@
 public class Main {
-public static void main(String[] args) {
-    int deposit = 15000;
-    int total = 0;
-    int m = 1;
-    while (total < 2459000){
-    total = total + total / 100;
-    total = total + deposit;
-    System.out.println(" Месяц " + m ++ + ", сумма накоплений равна " + total + " рублей ");}
+    public static void main(String[] args) {
+        int deposit = 1500;
+        int total = 0;
+        int m = 1;
+        while (total < 2459000) {
+            total = total + total / 100;
+            total = total + deposit;
+            System.out.println(" Месяц " + m++ + ", сумма накоплений равна " + total + " рублей ");
+        }
+
+        int a = 0;
+        while (a <= 10) {
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.print(a++ + " ");
+            System.out.println(a++);
+        }
+
+        for (int b = 10; b >= 0; ) {
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.print(b-- + " ");
+            System.out.println(b--);
+        }
     }
 }
